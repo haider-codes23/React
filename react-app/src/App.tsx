41b20b747ad70c4a823913ad457666380211b792
haider-codes23@github.com <haider.codes23@gmail.com>
2023-05-09 18:11:28 +0500

    Initial Commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,18 @@
+// import Message Object from message.tsx module
+// import Message from "./Message";
+
+// import ListGroup component from ListGroup.tsx
+import ListGroup from "./assets/components/ListGroup";
+
+// function App() {
+//   return <div><Message></Message></div>;
+// }
+
+// Just like the Message Component we should export the App Component as a default ojbect
+// export default App;
+
+function App() {
+  return <div><ListGroup></ListGroup></div>;
+}
+
+export default App;
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+  // JSX: JavaScript XML - JSX allows us to create dynimic content.
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
