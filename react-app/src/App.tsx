a2497fd50de797fefe3ad24d0474109fc8b222f4
haider-codes23@github.com <haider.codes23@gmail.com>
2023-05-11 12:33:56 +0500

    Biult an Alert Component that is dismissible by a click of a close button and visible in a click of another button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,6 +5,7 @@
 import ListGroup from "./assets/components/ListGroup";
 import Alert from "./assets/components/Alerts";
 import Button from "./assets/components/Button";
+import { useState } from "react";
 
 // function App() {
 //   return <div><Message></Message></div>;
@@ -19,8 +20,16 @@ import Button from "./assets/components/Button";
 function App() {
   // let items = ["New York", "San francisco", "Tokyo", "London", "Paris"];
   // const handleSelectItem = (item: string) => console.log(item);
+  // <div><Button color="secondary" onClick={() => console.log('Clicked')}>My Button</Button></div>;
 
-  return <div><Button color="secondary" onClick={() => console.log('Clicked')}>My Button</Button></div>;
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+   return (
+    <div>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>Alert Me</Alert>}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>My Button</Button>
+    </div>
+    );
 }
 
 export default App;
--- react-app/src/assets/components/Alerts.tsx
+++ react-app/src/assets/components/Alerts.tsx
@@ -1,12 +1,24 @@
+import { ReactNode } from "react";
+
 interface props {
   //text:string;
-  children: string;
+  children: ReactNode;
+  onClose: () => void;
+
 }
 
-const Alert = ({children}: props) => {
+const Alert = ({ children, onClose }: props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
-  )
-}
+    <div className="alert alert-primary alert-dimissible">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        onClick={onClose}
+        aria-label="Close"
+      ></button>
+    </div>
+  );
+};
 
 export default Alert;
