0c30befdc1a9f5d20c0e7710fd520a5180f5557f
haider-codes23@github.com <haider.codes23@gmail.com>
2023-05-13 21:01:59 +0500

    Managing Forms with react hook

--- react-app/src/assets/components/Form.tsx
+++ react-app/src/assets/components/Form.tsx
@@ -1,32 +1,26 @@
-import React, { FormEvent, useRef} from 'react';
+import React, { FormEvent, useRef, useState} from 'react';
+import { useForm } from 'react-hook-form';
 
+// Accessing the input fields using State Hooks
 const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = {
-    name: '',
-    age: 0,
-  }
-
-  const handleSubmission = (event: FormEvent) => {
-    event.preventDefault();
-    // console.log('Submitted')
-    if (nameRef.current !== null && ageRef.current !== null) {
-      person.name = nameRef.current.value;
-      person.age = parseInt(ageRef.current.value);
-      console.log(person);
-    };
-  };
+  // const form = useForm();
+  const {register, handleSubmit} = useForm();
+  console.log(register('name'));
+  
+  // const handleSubmission = (event: FormEvent) => {
+  //   event.preventDefault();
+  //   console.log(person);
+  // };
 
   return (
-    <form onSubmit={handleSubmission}>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input ref={nameRef} id='name' className="form-control"></input>
+        <input id='name' {...register('name')} className="form-control"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input ref={ageRef} id='age' type="number" className="form-control" />
+        <input id='age' {...register("age")} type="number" className="form-control" />
       </div>
       <button className='btn btn-primary'>Submit</button>
     </form>
@@ -34,3 +28,43 @@ const Form = () => {
 }
 
 export default Form;
+
+
+
+
+
+// Accessing Input field using Ref Hook and then storing those values in an object for send to the server
+// const Form = () => {
+//   const nameRef = useRef<HTMLInputElement>(null);
+//   const ageRef = useRef<HTMLInputElement>(null);
+//   const person = {
+//     name: '',
+//     age: 0,
+//   }
+
+//   const handleSubmission = (event: FormEvent) => {
+//     event.preventDefault();
+//     // console.log('Submitted')
+//     if (nameRef.current !== null && ageRef.current !== null) {
+//       person.name = nameRef.current.value;
+//       person.age = parseInt(ageRef.current.value);
+//       console.log(person);
+//     };
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmission}>
+//       <div className="mb-3">
+//         <label htmlFor="name" className="form-label">Name</label>
+//         <input ref={nameRef} id='name' className="form-control"></input>
+//       </div>
+//       <div className="mb-3">
+//         <label htmlFor="age" className="form-label">Age</label>
+//         <input ref={ageRef} id='age' type="number" className="form-control" />
+//       </div>
+//       <button className='btn btn-primary'>Submit</button>
+//     </form>
+//   );
+// }
+
+// export default Form;
