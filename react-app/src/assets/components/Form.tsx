0a1dea49c79772670d22cc0b4e0ca454b96715d1
haider-codes23@github.com <haider.codes23@gmail.com>
2023-05-13 14:08:15 +0500

    Accessing input fields and storing the values of the input field in an object that we send to the server

--- react-app/src/assets/components/Form.tsx
+++ react-app/src/assets/components/Form.tsx
@@ -1,20 +1,32 @@
-import React, { FormEvent } from 'react';
+import React, { FormEvent, useRef} from 'react';
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = {
+    name: '',
+    age: 0,
+  }
+
   const handleSubmission = (event: FormEvent) => {
     event.preventDefault();
-    console.log('Submitted')
+    // console.log('Submitted')
+    if (nameRef.current !== null && ageRef.current !== null) {
+      person.name = nameRef.current.value;
+      person.age = parseInt(ageRef.current.value);
+      console.log(person);
+    };
   };
 
   return (
     <form onSubmit={handleSubmission}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">Name</label>
-        <input id='name' className="form-control"></input>
+        <input ref={nameRef} id='name' className="form-control"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input id='age' type="number" className="form-control" />
+        <input ref={ageRef} id='age' type="number" className="form-control" />
       </div>
       <button className='btn btn-primary'>Submit</button>
     </form>
